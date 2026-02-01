878ba36797d4757f5a49687b4855287fabae0041
Shreyansh Soni <shreyanshsoni07@gmail.com>
2026-02-01 18:15:39 +0530

    Implement ESlint and Prettier

--- src/app.js
+++ src/app.js
@@ -2,8 +2,8 @@ import express from 'express';
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.status(200).send("Hello From Acquisition!");
-})
+app.get('/', (req, res) => {
+  res.status(200).send('Hello From Acquisition!');
+});
 
 export default app;
